6d2077bf69f25941758260446910701a4e7ed2a5
sikang hu <sikang_hu@berkeley.edu>
2019-07-25 18:59:31 -0400

    Updated README

--- src/IMS/Model/Order.java
+++ src/IMS/Model/Order.java
@@ -1,17 +1,14 @@
 package IMS.Model;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Created by Sikang on 2019-07-25.
