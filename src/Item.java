31c7bfdcf7548fc7530da320ed68338b7196941d
damonchen6886 <gu.che@husky.neu.edu>
2019-07-25 17:11:21 -0400

    category

---
+++ src/Item.java
@@ -0,0 +1,55 @@
+public class Item {
+  private int itemId;
+  private int categoryID;
+  private String itemName;
+  private int itemPrice;
+
+  public Item(int itemId, int categoryID, String itemName, int itemPrice){
+    this.itemId = itemId;
+    this.categoryID = categoryID;
+    this.itemName = itemName;
+    this.itemPrice = itemPrice;
+  }
+
+  @Override
+  public String toString() {
+    return "Item{" +
+            "itemId=" + itemId +
+            ", categoryID='" + categoryID + '\'' +
+            ", itemName='" + itemName + '\'' +
+            ", itemPrice=" + itemPrice +
+            '}';
+  }
+
+
+  public int getItemId() {
+    return itemId;
+  }
+
+  public void getItemId(int itemId) {
+    this.itemId = itemId;
+  }
+
+  public int getCategoryID() {
+    return categoryID;
+  }
+
+  public void setCategoryID(int categoryID) {
+    this.categoryID = categoryID;
+  }
+
+  public String getItemName() {
+    return itemName;
+  }
+
+  public void setItemName(String itemName) {
+    this.itemName = itemName;
+  }
+
+  public int getItemPrice(){
+    return itemPrice;
+  }
+  public void setItemPrice(int itemPrice){
+    this.itemPrice = itemPrice;
+  }
+}
---
+++ src/category.java
@@ -0,0 +1,43 @@
+public class category {
+  private int categoryID;
+  private String categoryName;
+  private String categoryDescription;
+
+  public category(int categoryID, String categoryName, String categoryDescription){
+    this.categoryID = categoryID;
+    this.categoryName = categoryName;
+    this.categoryDescription = categoryDescription;
+  }
+
+  @Override
+  public String toString() {
+    return "category{" +
+            "categoryID=" + categoryID +
+            ", categoryName='" + categoryName + '\'' +
+            ", categoryDescription='" + categoryDescription + '\'' +
+            '}';
+  }
+
+  public int getcategoryID() {
+    return categoryID;
+  }
+
+  public void setcategoryID(int categoryID) {
+    this.categoryID = categoryID;
+  }
+
+  public String getCategoryName() {
+    return categoryName;
+  }
+
+  public void setCategoryName(String categoryName) {
+    this.categoryName = categoryName;
+  }
+
+  public String getCategoryDescription(){
+    return categoryDescription;
+  }
+  public void setCategoryDescription(String categoryDescription){
+    this.categoryDescription = categoryDescription;
+  }
+}
