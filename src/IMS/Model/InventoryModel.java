b1ab31996fe1a3088c10db571cb2f1711ecb6772
andyh1109 <46579281+andyh1109@users.noreply.github.com>
2019-08-08 15:59:51 -0400

    Merge branch 'HaoYuan2' of https://github.com/Sikang-Hu/Inventory-Management-System into HaoYuan
    # Conflicts:
    #	src/IMS/IMSUtil.java
    #	src/IMS/Model/DBUtils.java
    #	src/IMS/Model/DatabaseSQL.java
    #	src/IMS/Model/IDatabaseSQL.java

--- src/IMS/Model/InventoryModel.java
+++ src/IMS/Model/InventoryModel.java
@@ -65,6 +65,30 @@ public interface InventoryModel {
 
   SaleDTO getSaleByID(int saleID);
 
+  //reorder related
+  List<ReorderPoint.reorderObject> getReminderByItemId(int itemId);
 
+  List<ReorderPoint.reorderObject> getReminderByStoreId(int storeId);
 
-}
+  List<ReorderPoint.reorderObject> getReminderByItemIdAndStoreId(int itemId, int storeId);
+
+  List<ReorderPoint.reorderObject> getAllReminders();
+
+  //Inventory age related
+  List<InventoryAge.inventoryAgeObject> getAgeByItemId(int itemId);
+
+  List<InventoryAge.inventoryAgeObject> getAgeByStoreId(int storeId);
+
+  List<InventoryAge.inventoryAgeObject> getAgeByItemIdAndStoreId(int itemId, int storeId);
+
+  List<InventoryAge.inventoryAgeObject> getAllAges();
+
+  //Turnover related
+  List<Turnover.turnoverObject> getTurnoverByItemId(int itemId, int numPastWeek);
+
+  List<Turnover.turnoverObject> getTurnoverByStoreId(int storeId, int numPastWeek);
+
+  List<Turnover.turnoverObject> getTurnoverByItemIdAndStoreId(int itemId, int storeId, int numPastWeek);
+
+  List<Turnover.turnoverObject> getAllTurnovers(int numPastWeek);
+  }
--- src/IMS/Model/ReorderPoint.java
+++ src/IMS/Model/ReorderPoint.java
@@ -87,7 +87,4 @@ public class ReorderPoint {
     }
 
 
-    public static void main(String[] args) {
-
-    }
 }
